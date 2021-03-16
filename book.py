077d92b1bdfa0896f3943362a4c2abba329323df
Thomas Broun <t.broun@me.com>
2021-03-16 16:21:42 +0100

    visualisation implemented

--- book.py
+++ book.py
@@ -1,4 +1,5 @@
 import itertools
+import pandas as pd
 
 class Book:
 
@@ -12,9 +13,13 @@ class Book:
         print("--- Insert BUY", amount, "@", price,
               "id=", neworder.idd, "on", self.name)  # print transaction
 
+        detailsorder = "--- Insert BUY ", str(amount), " @ ", str(price), " id=", str(neworder.idd), " on ", self.name
+
+
         self.orders.sort(key=lambda x: x.price, reverse=True)
 
         orderstodelete = []
+        executestring = [] 
 
         for order in self.orders:
             if (neworder.price >= order.price):
@@ -24,12 +29,19 @@ class Book:
                     if (amountleftinbook > 0):
                         print("Execute", neworder.amount, "at",
                               order.price, "on", self.name)
+                        executestr = "Execute " + str(neworder.amount) + " at " + str(order.price) + " on " + self.name
+                        newstring = ''.join(executestr)
+                        executestring.append(newstring)
+
                         order.amount = amountleftinbook
                         neworder.amount = 0
                         break
                     if (amountleftinbook == 0):
                         print("Execute", neworder.amount, "at",
                               order.price, "on", self.name)
+                        executestr = "Execute " + str(neworder.amount) + " at " + str(order.price) + " on " + self.name
+                        newstring = ''.join(executestr)
+                        executestring.append(newstring)
                         self.orders.remove(order)
                         neworder.amount = 0
                         break
@@ -37,6 +49,9 @@ class Book:
                     else:  # not enough amount in book
                         print("Execute", order.amount, "at",
                               order.price, "on", self.name)
+                        executestr = "Execute " + str(order.amount) + " at " + str(order.price) + " on " + self.name
+                        newstring = ''.join(executestr)
+                        executestring.append(newstring)
                         neworder.amount = neworder.amount - order.amount
                         order.amount = 0
                         orderstodelete.append(order)
@@ -50,14 +65,56 @@ class Book:
 
         print("Book on", self.name)
 
-        for order in self.orders:  # print the book
+        buyordertypes = []
+        buyorderamounts = []
+        buyorderprices = []
+        buyorderidds = []
+
+        sellordertypes = []
+        sellorderamounts = []
+        sellorderprices = []
+        sellorderidds = []
+
+        for order in self.orders:
             print("\t", order.ordertype, order.amount,
                   "@", order.price, "id=", order.idd)
+            if order.ordertype == "BUY":
+                buyordertypes.append(order.ordertype)
+                buyorderamounts.append(order.amount)
+                buyorderprices.append(order.price)
+                buyorderidds.append(order.idd)
+            if order.ordertype == "SELL":
+                sellordertypes.append(order.ordertype)
+                sellorderamounts.append(order.amount)
+                sellorderprices.append(order.price)
+                sellorderidds.append(order.idd)
 
-        print("----------------")
+        buyside = {'Type': buyordertypes, 'Amount': buyorderamounts, 'Price': buyorderprices, 'Id': buyorderidds}
+        sellside = {'Type': sellordertypes, 'Amount': sellorderamounts, 'Price': sellorderprices, 'Id': sellorderidds}
+
+        dfbuy = pd.DataFrame(data=buyside)
+        dfsell = pd.DataFrame(data=sellside)
+        print()
+        if dfbuy.empty:
+            print()
+            print("Empty buy book")
+        else:
+            print("-------- print buy side --------")
+            print(dfbuy)
+        
+        if dfsell.empty:
+            print("Empty sell book")
+        else:
+            print("-------- print sell side --------")
+            print(dfsell)
+        print()
+
+
 
     def insert_sell(self, amount, price):
         neworder = Order("SELL", amount, price)  # Create a new order
+
+        detailsorder = "--- Insert SELL ", str(amount), " @ ", str(price), " id=", str(neworder.idd), " on ", self.name
 
         print("--- Insert SELL", amount, "@", price,
               "id=", neworder.idd, "on", self.name)  # print transaction
@@ -65,6 +122,7 @@ class Book:
         self.orders.sort(key=lambda x: x.price, reverse=True)
 
         orderstodelete = []
+        executestring = [] 
 
         for order in self.orders:
             if (order.price >= neworder.price):
@@ -72,18 +130,28 @@ class Book:
                     amountleftinbook = order.amount - neworder.amount
                     # tout a ete execute / le book avait plus de stock que le nouvel ordre
                     if (amountleftinbook > 0):
+                        executestr = "Execute " + str(neworder.amount) + " at " + str(order.price) + " on " + self.name
+                        newstring = ''.join(executestr)
+                        executestring.append(newstring)
                         print("Execute", neworder.amount, "at",
                               order.price, "on", self.name)
                         order.amount = amountleftinbook
                         neworder.amount = 0
                         break
                     if (amountleftinbook == 0):
+                        executestr = "Execute " + str(neworder.amount) + " at " + str(order.price) + " on " + self.name
+                        newstring = ''.join(executestr)
+                        executestring.append(newstring)
                         print("Execute", neworder.amount, "at",
                               order.price, "on", self.name)
                         self.orders.remove(order)
                         break
 
                     else:  # not enough amount in book
+
+                        executestr = "Execute " + str(order.amount) + " at " + str(order.price) + " on " + self.name
+                        newstring = ''.join(executestr)
+                        executestring.append(newstring)
                         print("Execute", order.amount, "at",
                               order.price, "on", self.name)
                         neworder.amount = neworder.amount - order.amount
@@ -99,17 +167,60 @@ class Book:
 
         print("Book on", self.name)
 
+        buyordertypes = []
+        buyorderamounts = []
+        buyorderprices = []
+        buyorderidds = []
+
+        sellordertypes = []
+        sellorderamounts = []
+        sellorderprices = []
+        sellorderidds = []
+
         for order in self.orders:
             print("\t", order.ordertype, order.amount,
                   "@", order.price, "id=", order.idd)
+            if order.ordertype == "BUY":
+                buyordertypes.append(order.ordertype)
+                buyorderamounts.append(order.amount)
+                buyorderprices.append(order.price)
+                buyorderidds.append(order.idd)
+            if order.ordertype == "SELL":
+                sellordertypes.append(order.ordertype)
+                sellorderamounts.append(order.amount)
+                sellorderprices.append(order.price)
+                sellorderidds.append(order.idd)
 
-        print("----------------")
+        buyside = {'Type': buyordertypes, 'Amount': buyorderamounts, 'Price': buyorderprices, 'Id': buyorderidds}
+        sellside = {'Type': sellordertypes, 'Amount': sellorderamounts, 'Price': sellorderprices, 'Id': sellorderidds}
+
+        dfbuy = pd.DataFrame(data=buyside)
+        dfsell = pd.DataFrame(data=sellside)
+        print()
+        if dfbuy.empty:
+            print()
+            print("Empty buy book")
+        else:
+            print("-------- print buy side --------")
+            print(dfbuy)
+        
+        if dfsell.empty:
+            print("Empty sell book")
+        else:
+            print("-------- print sell side --------")
+            print(dfsell)
+        print()
 
     def addorder(self, order):
         self.orders.append(order)
 
+    def currentbook(self):
+        return self.orders
+        
+
 
 class Order:
+
     id_iter = itertools.count()
 
     def __init__(self, ordertype, amount, price):
